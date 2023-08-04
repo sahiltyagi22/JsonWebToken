b1eeed8a6de6796e9a8b7392416f5dcb7ca4d308
sahil <sahiltyagi1204@outlook.com>
2023-08-04 11:27:00 +0530

    adding jwt to login routes

--- index.js
+++ index.js
@@ -3,11 +3,42 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+const secretKey =  "thisisasecret"
+
 
 app.get('/' , (req,res)=>{
     res.send("yes its working")
 })
 
+app.post('/login' , (req,res)=>{
+    const user = {
+        id :"1",
+        name : "sahil",
+        password : "tygai"
+    }
+
+    jwt.sign({user} ,secretKey , {expiresIn : '300s'} , (err,token)=>{
+        res.json({
+            token
+        })
+    })
+})
+
+app.post("/profile" , verifyToken, (req,res)=>{
+
+})
+
+
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers['Authorization']
+    if( typeof bearerHeader !== 'undefined'){
+
+    }else{
+        res.send({
+            result : "this is not valid"
+        })
+    }
+}
 
 app.listen(3000 , ()=>{
     console.log("server is running");
